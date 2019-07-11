009aa0e1c487af9ab4aab1a179dd27a2f1f843a7
Artem Dereka <artemdereka@gmail.com>
2019-07-11 21:48:09 +0300

    added breadcrumbs

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,15 @@ var $ = require('jquery');
 var mixitup = require('mixitup');
 var slick = require('slick-carousel');
 
-var mixer = mixitup('.tabs__items-container');
+var mixer = mixitup('.tabs__items-container', {
+    load: {
+        filter: '.all'
+    }
+});
 
-$('.js-slider').slick();
+$('.js-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    arrows: false,
+    dots: true 
+});
