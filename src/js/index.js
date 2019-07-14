3f9eb8b89d8c816785eca2ec311bbc1291003d5e
Nataly Makarova <natmaklis@gmail.com>
2019-07-15 01:59:55 +0300

    tabs section and slider section. I couldn't do dots

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,15 @@ var $ = require('jquery');
  
 var mixitup = require('mixitup');
 var slick = require('slick-carousel');
-var mixer = mixitup('.tabs__items-container');
-$('.js-slider').slick();
+var mixer = mixitup('.tabs__items-container',{
+    load: {
+        filter: '.all'
+
+    }
+});
+$('.js-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    arrows: false,
+    dots : true
+});
