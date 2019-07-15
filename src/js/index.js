ce6019a6f6ecb462b4b498ed677584d464c5837d
Nataly Makarova <natmaklis@gmail.com>
2019-07-15 23:44:27 +0300

    dots

--- src/js/index.js
+++ src/js/index.js
@@ -2,15 +2,22 @@ var $ = require('jquery');
  
 var mixitup = require('mixitup');
 var slick = require('slick-carousel');
+var ionRangeSlider = require('ion-rangeslider');
+var slickNav = require('./jquery.slicknav.min.js');
 var mixer = mixitup('.tabs__items-container',{
     load: {
         filter: '.all'
 
     }
 });
+
 $('.js-slider').slick({
     slidesToShow: 4,
     slidesToScroll: 4,
     arrows: false,
     dots : true
 });
+
+
+
+
